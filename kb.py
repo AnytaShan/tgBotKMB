5df1fdc6ee1259908f631ea2152a379465d0fe43
AnytaShan <144763304+AnytaShan@users.noreply.github.com>
2024-12-20 09:34:21 +0300

    Update kb.py
    Обновление: клавиатура теперь выводится в два столбца

--- kb.py
+++ kb.py
@@ -1,14 +1,53 @@
-from aiogram.types import InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import db
 import day
 
-groups_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-    [button] for button in db.get_groups()
-])
+def create_groups_keyboard():
+    groups = db.get_groups()
+    # Создаем список для клавиатуры
+    inline_keyboard = []
+
+    # Группируем кнопки по две
+    for i in range(0, len(groups), 2):
+        row_buttons = groups[i:i + 2]  # Берем две кнопки за раз
+        inline_keyboard.append(row_buttons)  # Добавляем их в список как строку
+
+    # Создаем клавиатуру с правильным параметром
+    groups_keyboard = InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
+    return groups_keyboard
 
 
-dishes_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-    [button] for button in db.get_menu(day.get_DayOfTheWeekId())])
+def create_students_keyboard(group_id):
+    students = db.get_students(group_id)
+    # Создаем список для клавиатуры
+    inline_keyboard = []
+
+    # Группируем кнопки по две
+    for i in range(0, len(students), 2):
+        row_buttons = students[i:i + 2]  # Берем две кнопки за раз
+        inline_keyboard.append(row_buttons)  # Добавляем их в список как строку
+
+    # Создаем клавиатуру с правильным параметром
+    students_keyboard = InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
+    return students_keyboard
+
+
+btn_1 = InlineKeyboardButton(text='Да', callback_data='Yes')
+btn_2 = InlineKeyboardButton(text='Нет', callback_data='No')
+confirmation_kb = InlineKeyboardMarkup(inline_keyboard=[[btn_1], [btn_2]])
+
+
+
+def create_menu_keyboard():
+    menu = db.get_menu(day.get_DayOfTheWeekId())
+    menu_keyboard = []
+
+    for i in range(0, len(menu), 2):
+        row_buttons = menu[i:i + 2]
+        menu_keyboard.append(row_buttons)  
+
+    menu_keyboard = InlineKeyboardMarkup(inline_keyboard=menu_keyboard)
+    return menu_keyboard
 
 # клавиатура
 
