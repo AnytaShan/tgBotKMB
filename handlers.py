1e0ed64b7345645d90a4375a451ee8edddabf287
AnytaShan <144763304+AnytaShan@users.noreply.github.com>
2024-12-25 14:58:07 +0300

    Update handlers.py
    Добавлены сообщения для адменистрции

--- handlers.py
+++ handlers.py
@@ -1,7 +1,6 @@
 from aiogram import types, F, Router
 from aiogram.types import Message
 from aiogram.filters import Command
-from aiogram.types import  InlineKeyboardMarkup
 import text
 import kb
 import db
@@ -18,24 +17,34 @@ scheduler = AsyncIOScheduler()
 @router.message(Command("start"))
 async def start_handler(msg: Message):
     user_id = msg.from_user.id
-    if db.examination_start(user_id) == 1:
-        await msg.answer(f"Привет! А я уже знаком с тобой :)")
+    if db.get_admin(user_id) == 1:
+        await msg.answer(text= f"{msg.from_user.full_name}, вы зарегестрированы как админ. По будням в 9:00 я буду присылать статистику выбранных блюд")
     else:
-        await msg.answer(text.start)
-        db.get_groups()
-        await msg.answer(text.acquaintance.format(name=msg.from_user.full_name), reply_markup=kb.groups_keyboard)
+        if db.examination_start(user_id) == 1:
+            await msg.answer(f"Привет! А я уже знаком с тобой :)")
+        else:
+            await msg.answer(text.start)
+            db.get_groups()
+            await msg.answer(text.acquaintance.format(name=msg.from_user.full_name), reply_markup=kb.create_groups_keyboard())
+
 
 
 # Обработчик выбора группы
 @router.callback_query(lambda call: call.data.startswith("group_"))
 async def show_students(call: types.CallbackQuery):
-    group_id = call.data.split("_")[1]  # Извлекаем ID группы
-    student_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-    [button] for button in db.get_students(group_id)
-    ])
-    await call.message.answer(text.acquaintance_student.format(name=call.from_user.full_name), reply_markup=student_keyboard)
-    await main.delite(call.from_user.id, call.message.message_id)
-
+    user_id = call.from_user.id
+    if db.get_admin(user_id) == 1:
+        group_id = call.data.split("_")[1]  # Извлекаем ID группы
+        data = day.get_DayOfTheWeekId() - 1
+        text = db.admin_info(group_id, data)
+        text_message = "\n".join(text)
+        await call.message.answer(text_message)
+    else:
+        group_id = call.data.split("_")[1]  # Извлекаем ID группы
+        await call.message.answer(text.acquaintance_student.format(name=call.from_user.full_name), reply_markup=kb.create_students_keyboard(group_id))
+        await main.delite(call.from_user.id, call.message.message_id) 
+    
+   
 # Обработчик выбора студента
 @router.callback_query(lambda call: call.data.startswith("student_"))
 async def remember_student(call: types.CallbackQuery):
@@ -64,15 +73,20 @@ async def remember_student(call: types.CallbackQuery):
                 await call.message.answer(text.acquaintance_student2.format(name=call.from_user.full_name), reply_markup=kb.create_students_keyboard(group_id))
 
 
+
 # Обработчик выбора блюда
 @router.callback_query(lambda call: call.data.startswith("dishes_"))
 async def remember_delishe(call: types.CallbackQuery):    
     student_id = db.chat_student_id(call.from_user.id)
     dishes_id = call.data.split("_")[1]
     DayOfTheWeek_id = day.get_DayOfTheWeekId()
-    print(student_id, dishes_id, DayOfTheWeek_id)
     db.get_selection(student_id, dishes_id, DayOfTheWeek_id)
     await call.message.answer(text.thanks.format(name=call.from_user.full_name))
     await main.delite(call.from_user.id, call.message.message_id)
+
+
+@router.callback_query(lambda call: call.data.startswith("detailed"))
+async def show_students(call: types.CallbackQuery):
+    await call.message.answer(text="Выберете группу, подробности голосования которой хотите увидеть:",reply_markup=kb.create_groups_keyboard()) 
 
 
